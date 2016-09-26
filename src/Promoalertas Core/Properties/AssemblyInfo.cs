f99fe6580ad9e4b45de7d35ec90b3e0d404c9fe4
Carlos Pérez <antonio_-02@hotmail.com>
2016-09-26 11:48:43 -0500

    First Commit
    Se agrega el avance para el funcionamiento de la aplicación.

---
+++ src/Promoalertas Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Promoalertas_Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("81d86415-5b39-483d-8716-bd7f49307cd5")]
